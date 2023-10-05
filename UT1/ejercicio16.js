b7d39e6816cd7dcbb801262b0e30d67c0d701463
JaumeJD <jgarveg163@iesmartinezm.es>
2023-10-05 13:30:11 +0200

    Empezando ej16

---
+++ UT1/ejercicio16.js
@@ -0,0 +1,22 @@
+"use strict"
+
+/**
+ * Crea una función para dibujar un patrón de diente de sierra inverso en 
+ * un cuadro de texto. Con un carácter y un número que indique el mayor número
+ * de caracteres en la base (inversa) del patrón. pe1. Datos de entrada: 'A' y 5
+AAAAA
+AAAA
+AAA
+AA
+A
+ */
+
+let num = 5
+let caracter = "X"
+let patron = caracter.repeat(num)
+
+let dibujarPatron = (patron) => {
+
+}
+
+dibujarPatron(patron)
