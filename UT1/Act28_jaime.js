08d9427e7ab9d584310c20cf4b88027411267db5
JaumeJD <jgarveg163@iesmartinezm.es>
2023-10-19 00:04:52 +0200

    Act29 in process

--- UT1/Act28_jaime.js
+++ UT1/Act28_jaime.js
@@ -8,12 +8,57 @@ c. La cadena con más letras ‘a’.
 d. Cuántas cadenas hay que tengan la ‘b’ y cuáles son.
 */
 
-const vectorCadenas = ["diamante", "esternocleidomastoideo", "ola", "salto", "bollicao"]
+const vectorCadenas = ["diamante", "esternocleidomastoideo", "bola", "ola", "pasamontañas", "salto", "bollicao"]
 
 //A) La cadena más corta.
+{
+    let cadenaCorta = vectorCadenas[0]
+    vectorCadenas.forEach(cadena => {
+        if (cadena.length < cadenaCorta.length) {
+            cadenaCorta = cadena
+        }
+    });
+    console.log(`La cadena más corta es ${cadenaCorta}`)
+}
 
 //B) La cadena más larga.
+{
+    let cadenaLarga = vectorCadenas[0]
+    vectorCadenas.forEach(cadena => {
+        if (cadena.length > cadenaLarga.length) {
+            cadenaLarga = cadena
+        }
+    });
+    console.log(`La cadena más larga es ${cadenaLarga}`)
+}
 
 //C) La cadena con más letras 'a'.
+{
+    let contarLetrasA = vectorCadenas => {
+        let cadenaMasA = vectorCadenas[0]
+
+        vectorCadenas.forEach(cadena => {
+            if ((cadena.split('a').length - 1) > cadenaMasA.split('a').length - 1) {
+                cadenaMasA = cadena
+            }
+        });
+
+        return cadenaMasA
+    }
+    console.log(`La cadena con más 'a' es ${contarLetrasA(vectorCadenas)} con ${contarLetrasA(vectorCadenas).split('a').length - 1} a.`)
+}
 
 //D) Cuántas cadenas hay que tengan la 'b' y cuáles son.
+{
+    let contieneLetraB = vectorCadenas => {
+        let cadenasConB = []
+        vectorCadenas.forEach(cadena => {
+            if (cadena.includes("b")) {
+                cadenasConB.push(cadena)
+            }
+        })
+        return cadenasConB
+    }
+    console.log(`Hay ${contieneLetraB(vectorCadenas).length < 2 ? contieneLetraB(vectorCadenas).length + ' cadena:' : contieneLetraB(vectorCadenas).length + ' cadenas:'} 
+    ${contieneLetraB(vectorCadenas)}`)
+}
