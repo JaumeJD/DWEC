963999171fa8d89c52e11f6a99175dc96a03ddcc
JaumeJD <jgarveg163@iesmartinezm.es>
2023-12-02 21:52:21 +0100

    Process Act06

--- UT2/dom5.js
+++ UT2/dom5.js
@@ -4,5 +4,6 @@ let coord = document.createElement('p')
 coord.textContent = `Posición actual del mouse.`
 document.addEventListener('mousemove', function (e) {
     coord.textContent = `Eje X: ${e.offsetX} | Eje Y: ${e.offsetY}`
+    //coord.textContent = `Eje X: ${e.clientX} | Eje Y: ${e.clientY}`
 })
 document.body.appendChild(coord)
