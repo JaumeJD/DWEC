3c9a3c80ca8f7768606b36a8fe3fc29544920198
JaumeJD <jgarveg163@iesmartinezm.es>
2023-12-08 18:29:13 +0100

    apartado 5 y 6

--- UT2/dom6.js
+++ UT2/dom6.js
@@ -1,1 +1,26 @@
 "use strict"
+
+//Creacion del boton 'Comenzar Saludos', se añade estilo, 
+//el contenido de la etiqueta y la funcion que lance el setInterval.
+let iniciar = document.createElement('button')
+let saludo
+iniciar.setAttribute('style', 'padding: 10px; border-radius: 10px; background-color: aquamarine; font-weight: bold; margin-right: 10px;')
+iniciar.textContent = 'Comenzar Saludos'
+iniciar.addEventListener('click', function (event) {
+    saludo = setInterval(function () {
+        alert("HOLA CARACOLA")
+    }, 2000)
+})
+//Se añade el elemento boton "iniciar" al body del html.
+document.body.appendChild(iniciar)
+
+//Creacion del boton 'Parar Saludos', se añade estilo,
+//el contenido de la etiqueta y la funcion que pare la secuencia iniciada.
+let parar = document.createElement('button')
+parar.setAttribute('style', 'padding: 10px; border-radius: 10px; color: white; background-color: brown; font-weight: bold;')
+parar.textContent = 'Parar Saludos'
+parar.addEventListener('click', function (event) {
+    clearInterval(saludo)
+})
+//Se añade el elemento boton "parar" al body del html.
+document.body.appendChild(parar)
