4da6e8677e230f3ebf4aac6a4e581158701b6326
JaumeJD <jgarveg163@iesmartinezm.es>
2024-01-12 21:25:01 +0100

    Cookie en proceso, añadidas funciones

--- UT3/UT3_TAR02/cookies.js
+++ UT3/UT3_TAR02/cookies.js
@@ -1,20 +1,43 @@
 "use strict"
 
-let cont = 0
-
-if (cont == 0) {
-    cont++
-    document.cookie = `username=John Smith; expires= Thu, 18 Jan 2024 12:00:00 UTC; path=/; Nº de visitas: ${cont}`
-    console.log(document.cookie = `username=John Smith; expires= Thu, 18 Jan 2024 12:00:00 UTC; path=/; Nº de visitas: ${cont}`)
+//Función por defecto para crear cookies.
+let setCookie = (cname, cvalue, exdays) => {
+    let d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000))
+    let expires = "expires=" + d.toUTCString()
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/"
 }
 
-cont++
-console.log(document.cookie = `username=John Smith; expires= Thu, 18 Jan 2024 12:00:00 UTC; path=/; Nº de visitas: ${cont}`)
+//Función por defecto para consultar cookies.
+let getCookie = (cname) => {
+    let name = cname + "="
+    let ca = document.cookie.split(';')
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i]
+        while (c.charAt(0) == " ") {
+            c = c.substring(1)
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length)
+        }
+    }
+    return ""
+}
 
-let boton = document.createElement("button")
-boton.textContent = "Eliminar cookie"
-boton.addEventListener("click", function (event) {
-    document.cookie = `username=John Smith; expires= Thu, 18 Jan 2014 12:00:00 UTC; path=/; Nº de visitas: ${cont}`
-    console.log(document.cookie)
-})
-document.body.appendChild(boton)
+//Función por defecto para eliminar cookies.
+let deleteCookie = (cname) => {
+    document.cookie = cname + "=; expires=Thu, 01 JAN 1970 00:00:01 GMT;path=/"
+}
+
+//Función para comprobar si existe una cookie, si no, la establece.
+let checkCookie = () => {
+    let user = getCookie("username")
+    if (user != "") {
+        alert(`Welcome again ${user}`)
+    } else {
+        user = prompt("Please enter your name:", "")
+        if (user !== "" && user !== null) {
+            setCookie("username", user, 365)
+        }
+    }
+}
