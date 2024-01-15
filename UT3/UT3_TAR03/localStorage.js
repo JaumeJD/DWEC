bd00dc90e0b437a2ba598b327630e641f98677fd
JaumeJD <jgarveg163@iesmartinezm.es>
2024-01-15 02:39:00 +0100

    TAR03 Y 04

--- UT3/UT3_TAR03/localStorage.js
+++ UT3/UT3_TAR03/localStorage.js
@@ -1,1 +1,28 @@
 "use strict"
+
+let info = document.createElement("p")
+
+let comprobarLocalStorage = () => {
+    let visitCount = localStorage.getItem("visit")
+
+    if (visitCount == null) {
+        visitCount = 1
+    } else {
+        visitCount = parseInt(visitCount) + 1
+    }
+
+    localStorage.setItem("visit", visitCount)
+
+    info.textContent = `Nº de visitas: ${visitCount}`
+    document.body.appendChild(info)
+}
+
+document.addEventListener("DOMContentLoaded", comprobarLocalStorage)
+
+let button = document.createElement("button")
+button.textContent = "Eliminar contador"
+button.addEventListener("click", function (event) {
+    localStorage.removeItem("visit")
+    alert("Se ha eliminado el contador.")
+})
+document.body.appendChild(button)
